1340ef28d6780d76bf36455133d513f3318b68ba
Ella Buxton <82925463+ellabuxton@users.noreply.github.com>
2023-12-08 13:57:48 -0700

    Flagstaff Weather and FizzBuzz

---
+++ BuxtonChapter16/R/Flagstaff_Weather.R
@@ -0,0 +1,12 @@
+#' A time series of daily weather in Flagstaff, AZ.
+#'
+#' @format a data frame with 22967 observations and 5 variables
+#' \describe{
+#'   \item{DATE}{The date of observation as a POSIX date format.}
+#'   \item{RAIN}{Daily precipitation in inches.}
+#'   \item{SNOW}{Daily snowfall in inches.}
+#'   \item{TMAX}{Daily maximum temperature in degrees Farhenheit.}
+#'   \item{TMIN}{Daily minimum temperature in degrees Farhenheit.}
+#' }
+#' @source \url{https://www.ncdc.noaa.gov/cdo-web/search}
+"Flagstaff_Weather"
